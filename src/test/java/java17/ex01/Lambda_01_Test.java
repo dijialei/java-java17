1733f13487ab8ff7593307ca9e81fcf032634975
dijialei <dijialei0605@gmail.com>
2023-06-29 12:56:56 +0200

    tp02 Lambda interface

--- src/test/java/java17/ex01/Lambda_01_Test.java
+++ src/test/java/java17/ex01/Lambda_01_Test.java
@@ -1,5 +1,6 @@
 package java17.ex01;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java17.data.Data;
@@ -23,10 +24,10 @@ public class Lambda_01_Test {
     // tag::filter[]
     private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
         List<Person> filteredPersons = new ArrayList<Person>();
-    	for (Person p: persons){
-        	if (predicate.test(p)){
-        		filteredPersons.add(p);
-        	}
+        for (Person p : persons) {
+            if (predicate.test(p)) {
+                filteredPersons.add(p);
+            }
         }
         return filteredPersons;
     }
@@ -40,7 +41,8 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+        PersonPredicate predicate = p -> p.getAge() >= 18;
+        List<Person> result = filter(personList, predicate);
 
         assert result.size() == 83;
 
@@ -57,7 +59,8 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        PersonPredicate predicate = p -> p.getFirstname().equals("first_10");
+        List<Person> result = filter(personList, predicate);
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -75,7 +78,9 @@ public class Lambda_01_Test {
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        PersonPredicate predicate = p -> p.getAge() > 49 &&
+                DigestUtils.sha512Hex(p.getPassword()).equals("ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff");
+        List<Person> result = filter(personList, predicate);
 
         assert result.size() == 6;
         for (Person person : result) {
